7b9b9baeb91450ae8b5a7ad39fe39ea0037ff166
ehpark <silbroeh@gmail.com>
2024-05-24 09:43:41 +0900

    feat : token 타입 생성
    1. 로그인 성공 시 전달할 token dto (access, refresh) 생성
    2. refresh token의 경우, 서버 db 저장이 필요하므로 entity 생성

--- BE/src/main/java/com/codesquad/team3/issuetracker/domain/member/dto/response/MemberInfoResponse.java
+++ BE/src/main/java/com/codesquad/team3/issuetracker/domain/member/dto/response/MemberInfoResponse.java
@@ -1,7 +1,6 @@
 package com.codesquad.team3.issuetracker.domain.member.dto.response;
 
 import com.codesquad.team3.issuetracker.domain.member.entity.Member;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,16 +8,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class MemberInfoResponse {
-    private Integer id;
     private String memberId;
     private String nickname;
-    private LocalDate birthday;
+    private LocalDateTime birthday;
     private LocalDateTime joinTime;
     private String email;
 
     public static MemberInfoResponse toResponse(Member member) {
-        return new MemberInfoResponse(member.getId(),
-            member.getMemberId(),
+        return new MemberInfoResponse(member.getMemberId(),
             member.getNickname(),
             member.getBirthday(),
             member.getJoinTime(),
