ccf773558bf04226e5d7038abe84fba54bdb3221
ehpark <silbroeh@gmail.com>
2024-05-26 14:52:23 +0900

    feat : jwt 설정 refactor && interceptor filter 추가

--- BE/src/main/java/com/codesquad/team3/issuetracker/domain/jwt/service/JwtService.java
+++
@@ -1,8 +0,0 @@
-package com.codesquad.team3.issuetracker.domain.jwt.service;
-
-public interface JwtService {
-    String validateAccessToken(String accessToken);
-
-    String validateRefreshToken(String refreshToken);
-
-}
