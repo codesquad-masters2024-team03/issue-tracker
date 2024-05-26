1153791eb2f162847aa74ff0ddfc1ebc37bba414
ehpark <silbroeh@gmail.com>
2024-05-26 14:52:23 +0900

    feat : jwt 설정 refactor && interceptor filter 추가

--- BE/src/main/java/com/codesquad/team3/issuetracker/config/WebConfig.java
+++ BE/src/main/java/com/codesquad/team3/issuetracker/config/WebConfig.java
@@ -1,9 +1,8 @@
 package com.codesquad.team3.issuetracker.config;
 
-import com.codesquad.team3.issuetracker.jwt.interceptor.JwtInterceptor;
+import com.codesquad.team3.issuetracker.jwt.interceptor.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,21 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
-    private JwtInterceptor jwtInterceptor;
+    private JwtRequestFilter jwtRequestFilter;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtInterceptor)
+        registry.addInterceptor(jwtRequestFilter)
             .addPathPatterns("/**")
             .excludePathPatterns("/members", "/members/login", "/members/refresh-token");
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://www.issuetracker.site", "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
-                .allowCredentials(true);
     }
 }
