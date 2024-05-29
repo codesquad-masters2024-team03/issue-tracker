f4fa3d60a375f68f92e9260d56ac154a0ada0912
kimjooyoung <kjy915875@gmail.com>
2024-05-29 12:15:59 +0900

    fix. CORS 설정

--- BE/src/main/java/com/codesquad/team3/issuetracker/config/WebConfig.java
+++ BE/src/main/java/com/codesquad/team3/issuetracker/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.codesquad.team3.issuetracker.config;
 import com.codesquad.team3.issuetracker.jwt.interceptor.JwtInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +18,14 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(jwtInterceptor)
             .addPathPatterns("/**")
             .excludePathPatterns("/members", "/members/login", "/members/refresh-token");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://issue-tracker-751196260.ap-northeast-2.elb.amazonaws.com", "http://www.issuetracker.site")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true);
     }
 }
