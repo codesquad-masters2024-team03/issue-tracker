f60768be8fa87c03391d0c2a18782006d7f4b53f
Park Eunhyeong <142463332+silxbro@users.noreply.github.com>
2024-05-30 16:48:11 +0900

    login (#40)
    * fix : 로그인 interceptor 임시 해제
    
    로그아웃 상태 접근 가능 설정
    
    * refactor : Member 도메인 구조 변경
    
    1. 칼럼 추가 : oauth id, image url, join method
    2. db not null 제약사항 제거 (password, image url, birthday)
    3. builder 추가
    4. 생성자 변경 : create member dto
    - builder를 사용하여 member entity 반환
    5. dto record 변경
    
    * fix : member id, nickname, join method, join time, email NOT NULL 제약조건 추가(원복)
    
    * feat : 중복 검사 및 회원정보 유무 검사 로직 추가
    
    - db 접근을 최소화하여 성능 향상
    
    * fix : access token 및 refresh token 비밀키(secret key) 변경
    
    - 임의 설정값 변경
    
    * fix : birthday column 타입 변경에 따른 버그 해결
    
    * feat : logout 기능 구현
    
    * refactor : jwt util 모든 토큰 생성 메서드 추가
    
    * add : webflux 의존성 추가
    
    * feat : OAuth 로그인 config 설정
    
    1. dto 및 profile 도메인 생성
    2. provider 설정
    
    * feat : OAuth 로그인 기능 구현
    
    * fix : member 테이블 ddl 변경
    
    1. email이 null인 경우 존재
    2. image url 크키 조정

--- BE/src/main/java/com/codesquad/team3/issuetracker/domain/member/service/MemberServiceImpl.java
+++ BE/src/main/java/com/codesquad/team3/issuetracker/domain/member/service/MemberServiceImpl.java
@@ -1,5 +1,7 @@
 package com.codesquad.team3.issuetracker.domain.member.service;
 
+import static com.codesquad.team3.issuetracker.support.enums.SoftDeleteSearchFlags.NOT_DELETED;
+
 import com.codesquad.team3.issuetracker.domain.member.dto.request.CreateMember;
 import com.codesquad.team3.issuetracker.domain.member.dto.request.LoginMember;
 import com.codesquad.team3.issuetracker.domain.member.dto.request.UpdateMember;
@@ -8,7 +10,6 @@ import com.codesquad.team3.issuetracker.domain.member.dto.response.MemberInfoRes
 import com.codesquad.team3.issuetracker.domain.member.entity.Member;
 import com.codesquad.team3.issuetracker.domain.member.repository.MemberRepository;
 import com.codesquad.team3.issuetracker.jwt.util.JwtUtil;
-import com.codesquad.team3.issuetracker.support.enums.SoftDeleteSearchFlags;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.naming.AuthenticationException;
@@ -23,53 +24,76 @@ public class MemberServiceImpl implements MemberService {
     private final JwtUtil jwtUtil;
 
     @Override
-    public MemberInfoResponse create(CreateMember createRequest) {
-        Member savedMember = memberRepository.insert(new Member(createRequest));
+    public MemberInfoResponse create(CreateMember createRequest) throws IllegalArgumentException {
+        if (memberRepository.existsByMemberId(createRequest.memberId())) {
+            throw new IllegalArgumentException("동일한 아이디가 이미 존재합니다.");
+        }
+        if (memberRepository.existsByNickname(createRequest.nickname())) {
+            throw new IllegalArgumentException("동일한 닉네임이 이미 존재합니다.");
+        }
+        if (memberRepository.existsByEmail(createRequest.email())) {
+            throw new IllegalArgumentException("동일한 이메일이 이미 존재합니다.");
+        }
+
+        Member savedMember = memberRepository.insert(createRequest.toMember());
         return MemberInfoResponse.toResponse(savedMember);
     }
 
     @Override
-    public MemberInfoResponse update(Integer targetId, UpdateMember updateRequest) {
-        Member targetMember = memberRepository.findByIdWithDeleteCondition(targetId, SoftDeleteSearchFlags.NOT_DELETED).get();
+    public MemberInfoResponse update(Integer targetId, UpdateMember updateRequest) throws IllegalArgumentException {
+        Member targetMember = memberRepository.findByIdWithDeleteCondition(targetId, NOT_DELETED)
+            .orElseThrow(() -> new IllegalArgumentException("회원정보가 없습니다."));
+        if (memberRepository.existsByNickname(updateRequest.nickname())) {
+            throw new IllegalArgumentException("동일한 닉네임이 이미 존재합니다.");
+        }
+        if (memberRepository.existsByEmail(updateRequest.email())) {
+            throw new IllegalArgumentException("동일한 이메일이 이미 존재합니다.");
+        }
+
         Member updatedMember = memberRepository.update(targetMember.update(updateRequest));
         return MemberInfoResponse.toResponse(updatedMember);
     }
 
     @Override
-    public MemberInfoResponse findById(Integer targetId) {
-        Member targetMember = memberRepository.findByIdWithDeleteCondition(targetId, SoftDeleteSearchFlags.NOT_DELETED).get();
+    public MemberInfoResponse findById(Integer targetId) throws IllegalArgumentException {
+        Member targetMember = memberRepository.findByIdWithDeleteCondition(targetId, NOT_DELETED)
+            .orElseThrow(() -> new IllegalArgumentException("회원정보가 없습니다."));
+
         return MemberInfoResponse.toResponse(targetMember);
     }
 
     @Override
-    public MemberInfoResponse findByMemberId(String memberId) {
-        Member targetMember = memberRepository.findByMemberId(memberId).get();
+    public MemberInfoResponse findByMemberId(String memberId) throws IllegalArgumentException {
+        Member targetMember = memberRepository.findByMemberId(memberId)
+            .orElseThrow(() -> new IllegalArgumentException("회원정보가 없습니다."));
+
         return MemberInfoResponse.toResponse(targetMember);
     }
 
     @Override
     public List<MemberInfoResponse> findAll() {
-        List<Member> allMembers = (List<Member>) memberRepository.findAll(SoftDeleteSearchFlags.NOT_DELETED);
+        List<Member> allMembers = (List<Member>) memberRepository.findAll(NOT_DELETED);
         return allMembers.stream().map(MemberInfoResponse::toResponse).collect(Collectors.toList());
     }
 
     @Override
-    public MemberInfoResponse softDeleteById(Integer targetId) {
-        Member targetMember = memberRepository.findByIdWithDeleteCondition(targetId, SoftDeleteSearchFlags.NOT_DELETED).get();
+    public MemberInfoResponse softDeleteById(Integer targetId) throws IllegalArgumentException {
+        Member targetMember = memberRepository.findByIdWithDeleteCondition(targetId, NOT_DELETED)
+            .orElseThrow(() -> new IllegalArgumentException("회원정보가 없습니다."));
+
         Member deletedMember = memberRepository.softDelete(targetMember);
         return MemberInfoResponse.toResponse(deletedMember);
     }
 
     @Override
     public TokenResponse login(LoginMember loginRequest) throws AuthenticationException {
-        Member targetMember = memberRepository.findByMemberId(loginRequest.getMemberId())
+        Member targetMember = memberRepository.findByMemberId(loginRequest.memberId())
             .orElseThrow(() -> new AuthenticationException("회원정보가 없습니다."));
-
-        if (!targetMember.checkPassword(loginRequest.getPassword())) {
+        if (!targetMember.checkPassword(loginRequest.password())) {
             throw new AuthenticationException("비밀번호가 일치하지 않습니다.");
-        };
+        }
 
-        TokenResponse newTokens = createTokens(targetMember.getMemberId());
+        TokenResponse newTokens = jwtUtil.createTokens(targetMember.getMemberId());
         targetMember.refreshToken(newTokens.refreshToken());
         memberRepository.update(targetMember);
 
@@ -77,24 +101,25 @@ public class MemberServiceImpl implements MemberService {
     }
 
     @Override
+    public void logout(String refreshToken) throws AuthenticationException {
+        Member targetMember = memberRepository.findByRefreshToken(refreshToken)
+            .orElseThrow(() -> new AuthenticationException("회원정보가 없습니다."));
+        targetMember.removeRefreshToken();
+        memberRepository.update(targetMember);
+    }
+
+    @Override
     public TokenResponse refreshToken(String refreshToken) throws AuthenticationException {
         Member targetMember = memberRepository.findByRefreshToken(refreshToken)
             .orElseThrow(() -> new AuthenticationException("회원정보가 없습니다."));
-
         if (!jwtUtil.validateRefreshToken(refreshToken, targetMember.getMemberId())) {
             throw new AuthenticationException("토큰이 유효하지 않습니다.");
         }
 
-        TokenResponse newTokens = createTokens(targetMember.getMemberId());
+        TokenResponse newTokens = jwtUtil.createTokens(targetMember.getMemberId());
         targetMember.refreshToken(newTokens.refreshToken());
         memberRepository.update(targetMember);
 
         return newTokens;
-    }
-
-    private TokenResponse createTokens(String memberId) {
-        String accessToken = jwtUtil.createAccessToken(memberId);
-        String refreshToken = jwtUtil.createRefreshToken(memberId);
-        return new TokenResponse(accessToken, refreshToken);
     }
 }
